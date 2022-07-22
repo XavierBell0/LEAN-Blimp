274a3de419b3ec47459fad9fd299db22fcb4dc00
XavierBell0 <xavier.l.bell@gmail.com>
2022-07-22 18:03:53 -0400

    added second current sensor

--- testScripts/power sense.py
+++ testScripts/power sense.py
@@ -3,47 +3,72 @@ import board
 from adafruit_ina219 import ADCResolution, BusVoltageRange, INA219
 
 
-i2c_bus = board.I2C()
+i2c_pi = board.I2C()
 print(i2c_bus)
-ina219 = INA219(i2c_bus)
-pi_ina219 = INA219(0x41)
+pi_ina219 = INA219(i2c_bus)
+motor_ina219 = INA219(i2c, 0x41)
 
 print("ina219 test")
 
 # display some of the advanced field (just to test)
 print("Config register:")
-print("  bus_voltage_range:    0x%1X" % ina219.bus_voltage_range)
-print("  gain:                 0x%1X" % ina219.gain)
-print("  bus_adc_resolution:   0x%1X" % ina219.bus_adc_resolution)
-print("  shunt_adc_resolution: 0x%1X" % ina219.shunt_adc_resolution)
-print("  mode:                 0x%1X" % ina219.mode)
+print("  bus_voltage_range:    0x%1X" % pi_ina219.bus_voltage_range)
+print("  gain:                 0x%1X" % pi_ina219.gain)
+print("  bus_adc_resolution:   0x%1X" % pi_ina219.bus_adc_resolution)
+print("  shunt_adc_resolution: 0x%1X" % pi_ina219.shunt_adc_resolution)
+print("  mode:                 0x%1X" % pi_ina219.mode)
 print("")
 
 # optional : change configuration to use 32 samples averaging for both bus voltage and shunt voltage
-ina219.bus_adc_resolution = ADCResolution.ADCRES_12BIT_32S
-ina219.shunt_adc_resolution = ADCResolution.ADCRES_12BIT_32S
+pi_ina219.bus_adc_resolution = ADCResolution.ADCRES_12BIT_32S
+pi_ina219.shunt_adc_resolution = ADCResolution.ADCRES_12BIT_32S
+motor_ina219.bus_adc_resolution = ADCResolution.ADCRES_12BIT_32S
+motor_ina219.shunt_adc_resolution = ADCResolution.ADCRES_12BIT_32S
 # optional : change voltage range to 16V
-ina219.bus_voltage_range = BusVoltageRange.RANGE_16V
+pi_ina219.bus_voltage_range = BusVoltageRange.RANGE_16V
+motor_ina219.bus_voltage_range = BusVoltageRange.RANGE_16V
 
 # measure and display loop
 while True:
-    bus_voltage = ina219.bus_voltage  # voltage on V- (load side)
-    shunt_voltage = ina219.shunt_voltage  # voltage between V+ and V- across the shunt
-    current = ina219.current  # current in mA
-    power = ina219.power  # power in watts
+    pi_bus_voltage = pi_ina219.bus_voltage  # voltage on V- (load side)
+    pi_shunt_voltage = pi_ina219.shunt_voltage  # voltage between V+ and V- across the shunt
+    pi_current = pi_ina219.current  # current in mA
+    pi_power = pi_ina219.power  # power in watts
+
+    motor_bus_voltage = motor_ina219.bus_voltage  # voltage on V- (load side)
+    motor_shunt_voltage = motor_ina219.shunt_voltage  # voltage between V+ and V- across the shunt
+    motor_current = motor_ina219.current  # current in mA
+    motor_power = motor_ina219.power # power in watts
 
     # INA219 measure bus voltage on the load side. So PSU voltage = bus_voltage + shunt_voltage
-    print("Voltage (VIN+) : {:6.3f}   V".format(bus_voltage + shunt_voltage))
-    print("Voltage (VIN-) : {:6.3f}   V".format(bus_voltage))
-    print("Shunt Voltage  : {:8.5f} V".format(shunt_voltage))
-    print("Shunt Current  : {:7.4f}  A".format(current / 1000))
-    print("Power Calc.    : {:8.5f} W".format(bus_voltage * (current / 1000)))
-    print("Power Register : {:6.3f}   W".format(power))
+    print("Pi-side power data:")
+    print("Voltage (VIN+) : {:6.3f}   V".format(pi_bus_voltage + pi_shunt_voltage))
+    print("Voltage (VIN-) : {:6.3f}   V".format(pi_bus_voltage))
+    print("Shunt Voltage  : {:8.5f} V".format(pi_shunt_voltage))
+    print("Shunt Current  : {:7.4f}  A".format(pi_current / 1000))
+    print("Power Calc.    : {:8.5f} W".format(pi_bus_voltage * (pi_current / 1000)))
+    print("Power Register : {:6.3f}   W".format(pi_power))
     print("")
 
     # Check internal calculations haven't overflowed (doesn't detect ADC overflows)
-    if ina219.overflow:
+    if pi_ina219.overflow or motor_ina219.overflow:
         print("Internal Math Overflow Detected!")
         print("")
 
-    time.sleep(2)
+    time.sleep(1)
+
+    print("Motor-side power data:")
+    print("Voltage (VIN+) : {:6.3f}   V".format(motor_bus_voltage + motor_shunt_voltage))
+    print("Voltage (VIN-) : {:6.3f}   V".format(motor_bus_voltage))
+    print("Shunt Voltage  : {:8.5f} V".format(motor_shunt_voltage))
+    print("Shunt Current  : {:7.4f}  A".format(motor_current / 1000))
+    print("Power Calc.    : {:8.5f} W".format(motor_bus_voltage * (motor_current / 1000)))
+    print("Power Register : {:6.3f}   W".format(motor_power))
+    print("")
+
+    # Check internal calculations haven't overflowed (doesn't detect ADC overflows)
+    if pi_ina219.overflow or motor_ina219.overflow:
+        print("Internal Math Overflow Detected!")
+        print("")
+
+    time.sleep(1)
