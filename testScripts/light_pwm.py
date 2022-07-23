37e7d1415f66a68163842ad0bd2359b2fa66a944
XavierBell0 <xavier.l.bell@gmail.com>
2022-07-23 11:46:14 -0400

    added light_pwm

---
+++ testScripts/light_pwm.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)
+
+Motor1PWM = 12
+Motor1Dir = 5
+
+GPIO.setup(Motor1PWM, GPIO.OUT)
+GPIO.setup(Motor1Dir, GPIO.OUT)
+GPIO.output(Motor1PWM, GPIO.LOW)
+GPIO.output(Motor1Dir, GPIO.LOW)
+
+pwm = GPIO.PWM(Motor1PWM, 100)
+
+print("Turning motor 1 on")
+pwm1.start(20)
+sleep(3)
+print("Changing Direction")
+GPIO.output(Motor1Dir, GPIO.HIGH)
+sleep(3)
+pwm1.ChangeDutyCycle(100)
