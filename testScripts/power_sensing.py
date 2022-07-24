300fc8f89a55db85d80e6c7e7c0774382d339f3f
XavierBell0 <xavier.l.bell@gmail.com>
2022-07-24 09:25:08 -0400

    power averaging

--- testScripts/power_sensing.py
+++ testScripts/power_sensing.py
@@ -11,13 +11,13 @@ pi = INA219(i2c_bus, 0x40)
 motor = INA219(i2c_bus, 0x41)
 
 #change configuration to use 32 samples averaging for both bus voltage and shunt voltage
-pi.bus_adc_resolution = ADCResolution.ADCRES_12BIT_64S
-motor.bus_adc_resolution = ADCResolution.ADCRES_12BIT_64S
-#hange voltage range to 16V
+pi.bus_adc_resolution = ADCResolution.ADCRES_12BIT_32S
+motor.bus_adc_resolution = ADCResolution.ADCRES_12BIT_32S
+#change voltage range to 16V
 pi.bus_voltage_range = BusVoltageRange.RANGE_16V
 motor.bus_voltage_range = BusVoltageRange.RANGE_16V
 
-# measure and display loop
+#measure loop
 while True:
     msg = power_data()
     msg.pi_voltage = pi.bus_voltage  # voltage on V- (load side)
