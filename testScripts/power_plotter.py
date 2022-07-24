1c1b3b99eedb5f954397a044d586cecfe99eb95e
XavierBell0 <xavier.l.bell@gmail.com>
2022-07-24 09:37:26 -0400

    power avg sample size

--- testScripts/power_plotter.py
+++ testScripts/power_plotter.py
@@ -23,13 +23,14 @@ with sqlite3.connect(power_db) as c:
             current_y.append(entry[2])
             power_y.append(entry[3])
             time_x.append(entry[4])
+        samples = 10
         avg_power_y = []
-        for entry in range(len(power_y)-5):
-            sample = power_y[entry:entry + 5]
+        for entry in range(len(power_y)-samples):
+            sample = power_y[entry:entry + samples]
             avg_power_y.append(sum(sample)/len(sample))
         avg_current_y = []
-        for entry in range(len(current_y)-5):
-            sample = current_y[entry:entry + 5]
+        for entry in range(len(current_y)-samples):
+            sample = current_y[entry:entry + samples]
             avg_current_y.append(sum(sample)/len(sample))
 
         fig, (ax1, ax2, ax3) = plt.subplots(3, sharex=True)
@@ -37,11 +38,11 @@ with sqlite3.connect(power_db) as c:
         ax1.set_ylim([2, 5.25])
         ax1.set(xlabel = 'Time (s)', ylabel = "Voltage (V)")
         ax2.plot(np.array(time_x), np.array(current_y))
-        ax2.plot(np.array(time_x[:-5]), np.array(avg_current_y))
+        ax2.plot(np.array(time_x[:-samples]), np.array(avg_current_y))
         ax2.set_ylim([0, 1500])
         ax2.set(xlabel = 'Time (s)', ylabel = "Current (mA)")
         ax3.plot(np.array(time_x), np.array(power_y))
-        ax3.plot(np.array(time_x[:-5]), np.array(avg_power_y))
+        ax3.plot(np.array(time_x[:-samples]), np.array(avg_power_y))
         ax3.set_ylim([0, 5])
         ax3.set(xlabel = 'Time (s)', ylabel = "Power (W)")
         #running_power = []
